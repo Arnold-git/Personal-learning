051602037ca8be07a5e22e9621e40283708012f7
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-07-20 11:33:14 +0100

    updated app.js

--- node/app.js
+++ node/app.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) =>{
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
 });
 
 server.listen(3000);
