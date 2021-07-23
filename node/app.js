b11b35c19fbd0b9d7b5573a50a77dba560f26a10
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-07-23 21:08:58 +0100

    uodates

--- node/app.js
+++ node/app.js
@@ -8,20 +8,20 @@ const server = http.createServer((req, res) =>{
     if (url === '/') {
 
         res.write('<html>');
-        res.write('<head><title>My First Page</title><head>');
+        res.write('<head><title>Enter Message</title><head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        body = [];
+        const body = [];
         req.on('data', (chunk) => {
             console.log(chunk);
             body.push(chunk);    
         });
         req.on('end', () =>{
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
         });
         res.statusCode = 302;
