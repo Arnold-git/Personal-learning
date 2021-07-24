54497810c0b5913002673d9c57e0ddd444501a47
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-07-24 11:39:37 +0100

    api updates

--- express/app.js
+++ express/app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send('Hello World');
+    res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req, res) =>{
     res.send([1, 2, 3])
 });
-app.listen(3000, () => console.log('Listening on port 3000...'));
+
+app.get('/api/posts/:year/:month', (req, res) =>{
+    res.send(req.params);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(3000, function () {
+    console.log(`Listening on port ${port}...`)
+})
