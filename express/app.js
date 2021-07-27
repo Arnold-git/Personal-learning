2789a9ce4ea1ee5bc31764a90fe0a5d3cea82cc0
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-07-27 21:47:47 +0100

    updates

--- express/app.js
+++ express/app.js
@@ -51,6 +51,22 @@ app.put('/api/courses/:id', (req, res) =>{
 
 });
 
+
+app.delete('/api/courses/:id', (req, res) =>{
+    const course = courses.find(c =>c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send('The course with given ID was not found')
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1)
+
+    res.send(course);
+
+});
+
+
+
+
+
 function validateCourse(course) {
     const schema = {
         name: Joi.string().min(3).required()
