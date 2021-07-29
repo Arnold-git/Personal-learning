319d3c058c2aed7a645ff247a53d934e36b71528
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-07-29 08:39:04 +0100

    blog app initial push

---
+++ blog api/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config')
+
+const postRoute = require('./routes/posts');
+
+app.use('/posts', postRoute);
+
+
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(()=>{
+    console.log('Database connected')
+}).catch(err=>{
+    console.log('Database could not connect'+err);
+})
+
+
+app.get('/', (req, res) =>{
+    res.send('This is default route');
+});
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(3000, function () {
+    console.log(`Listening on port ${port}...`);
+});
