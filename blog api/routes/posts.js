897e4059b24093153f3edcbf04136c2c85586ef2
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-07-31 18:49:40 +0100

    api updates

--- blog api/routes/posts.js
+++ blog api/routes/posts.js
@@ -1,14 +1,16 @@
 const express = require('express');
-
 const router = express.Router();
+const Posts = require('../models/Post');
+
 
 router.get('/', (req, res) => {
     res.send('We are on posts');
 });
 
-router.get('/specific', (req, res) => {
-    res.send('Specific posts');
-});
 
+router.post('/', (req, res) =>{
+    console.log(req.body)
+
+});
 
 module.exports = router;
