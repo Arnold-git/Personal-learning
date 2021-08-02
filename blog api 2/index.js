bb1a5c94e75b73e10e79a541e968b573ee1f1b65
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-02 21:49:55 +0100

    initial commit

---
+++ blog api 2/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./app')
+
+
+const server = http.createServer(app);
+
+
+
+const port = process.env.PORT || 3000;
+server.listen(3000, function () {
+    console.log(`Listening on port ${port}...`);
+});
+
