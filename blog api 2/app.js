f1aaa281a7496d8b9a0180437ddae6a714d1e68a
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-02 22:10:23 +0100

    init

--- blog api 2/app.js
+++ blog api 2/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express();
 
+app.use('/user', (req, res, next) =>{
+    res.status(200).json({
+        message: "User request"
+    })
+})
+
 app.use('/', (req, res, next) =>{
     res.status(200).json({
         message: "Hello world"
     })
-})
+});
+
+
+
 
 
 module.exports = app;
