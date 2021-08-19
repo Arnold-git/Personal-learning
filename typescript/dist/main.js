f7595c050d9bf0d50f6a74034904e788dfe02586
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-19 22:00:13 +0100

    :memo: typescript

---
+++ typescript/dist/main.js
@@ -0,0 +1,12 @@
+// Functions
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("fy", "lesson"));
+// variables
+var hello = "world";
+hello = "foo";
+// let hello:string = "world"
+// hello = "boy"
+// let hello: string = "world"
+// hello = true
---
+++ typescript/src/main.ts
@@ -0,0 +1,20 @@
+
+
+
+// Functions
+
+const getFullName = (name:string, surname:string): string => {
+    return name + " " + surname;
+};
+
+console.log(getFullName("fy", "lesson"))
+
+// variables
+let hello:string = "world";
+hello = "foo"
+
+// let hello:string = "world"
+// hello = "boy"
+
+// let hello: string = "world"
+// hello = true
