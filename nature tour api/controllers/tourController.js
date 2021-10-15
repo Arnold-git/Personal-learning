a42383a08e59aa1c78e2cf6b2afb63fce113e2eb
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-15 12:43:17 +0100

    :rocket:

--- nature tour api/controllers/tourController.js
+++ nature tour api/controllers/tourController.js
@@ -1,7 +1,12 @@
 const Tour = require('./../models/tourModel')
 
 
-
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = "-ratingAverage,price";
+  req.query.fields = "name, price, ratingAverage, summary, difficulty";
+  next();
+}
 
 
 exports.getAllTours = async (req, res) => {
