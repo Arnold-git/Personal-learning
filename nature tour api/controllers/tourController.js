2268e58a7b31be76dfd58026336395ce4606368e
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-16 19:17:45 +0100

    :rocket:

--- nature tour api/controllers/tourController.js
+++ nature tour api/controllers/tourController.js
@@ -24,7 +24,7 @@ class APIFeatures {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
     
-    this.query.find(JSON.parse(queryStr))
+    this.query = this.query.find(JSON.parse(queryStr))
 
     return this
   }
@@ -41,6 +41,31 @@ class APIFeatures {
 
   }
 
+  limitFields() {
+    if(this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ")
+      this.query = query.select(fields)
+    } else {
+      this.query = this.query.select('-__v')
+    }
+
+    return this;
+  }
+
+  paginate(){
+
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit)
+    
+    return this;
+  }
+
+
+  const features = new APIFeatures(Tour.find(), req.query).filter()
+  const tours = await features.query;
   
 }
 
@@ -76,28 +101,28 @@ exports.getAllTours = async (req, res) => {
 
     // field limiting
 
-    if(req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ")
-      query = query.select(fields)
-    } else {
-      query = query.select('-__v')
-    }
+    // if(req.query.fields) {
+    //   const fields = req.query.fields.split(",").join(" ")
+    //   query = query.select(fields)
+    // } else {
+    //   query = query.select('-__v')
+    // }
     
     // pagination
 
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
 
-    query = query.skip(skip).limit(limit)
+    // query = query.skip(skip).limit(limit)
 
-    if (req.query.page) {
-      const numTours = await tours.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exit');
-    }
+    // if (req.query.page) {
+    //   const numTours = await tours.countDocuments();
+    //   if (skip >= numTours) throw new Error('This page does not exit');
+    // }
 
-    const features = new APIFeatures(Tour.find(), req.query).filter()
-    const tours = await features.query;
+    // const features = new APIFeatures(Tour.find(), req.query).filter()
+    // const tours = await features.query;
 
     res.status(200).json({
       status: "Success",
