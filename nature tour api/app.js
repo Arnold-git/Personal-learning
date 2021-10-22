036ceec1e854ce07528eef13c526d0ca34bf3f62
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-22 12:30:01 +0100

    ;rocket:

--- nature tour api/app.js
+++ nature tour api/app.js
@@ -32,8 +32,8 @@ app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
   res.status(404).json({
     status: 'fail',
-    message: "Can't find ${req.originalUrl}"
-  })
-})
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
 
 module.exports = app;
