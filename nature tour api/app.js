00066cb6b2c58c95e0da25edf62c5b190c2a7cb5
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-23 14:28:19 +0100

    added error handler :fire:

--- nature tour api/app.js
+++ nature tour api/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const AppError = require('./utils/appError')
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController')
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -33,24 +34,11 @@ app.use('/api/v1/users', userRouter);
 // unhandle route error handler
 app.all('*', (req, res, next) => {
 
-  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  // err.status = 'fail',
-  // err.statusCode = 404;
-
-  next(new AppError(`Can't find ${req.originalUrl} on this server`), 404);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 
 });
 
 
-app.use((err, req, res, next) => {
-
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-})
+app.use(globalErrorHandler)
 
 module.exports = app;
