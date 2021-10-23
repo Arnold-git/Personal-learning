8ad0335ad81c2ab780d93f724faab86aab4ef6ff
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-23 19:13:10 +0100

    modified async error handler :fire:

--- nature tour api/controllers/errorController.js
+++ nature tour api/controllers/errorController.js
@@ -2,9 +2,23 @@ module.exports = (err, req, res, next) => {
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
+
+    if(process.env.NODE_ENV === 'developemnt') {
+      res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+      });
+
+    } else if (process.env.NODE_ENV ==='production') {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+      
+
+    }
   
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
+
 }
