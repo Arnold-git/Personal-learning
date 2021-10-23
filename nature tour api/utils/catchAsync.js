606e19f281a3b52501edfa453bf072c980f8dd78
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-23 20:18:51 +0100

    modified async error handler :fire:

--- nature tour api/utils/catchAsync.js
+++ nature tour api/utils/catchAsync.js
@@ -1,7 +1,8 @@
 module.exports = fn => {
     // next help to catch the error
     return (req, res, next) => {
-      fn(req, res, next).catch(err => next(err))  
+      fn(req, res, next).catch(next)  
     };
 };
+
 
