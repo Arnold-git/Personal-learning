d7d984f37aacb01960bf84725588199280dcf866
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-24 16:35:44 +0100

    :rocket:

--- nature tour api/controllers/errorController.js
+++ nature tour api/controllers/errorController.js
@@ -2,9 +2,17 @@ module.exports = (err, req, res, next) => {
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+      
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+
+    } else if (process.env.NODE_ENV === 'Pproductionn') {
+
+    }
   
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
+    
 }
