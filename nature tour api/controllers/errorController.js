d55ebe6cefa49582e0e07da5b413cd0c5b9a51ef
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-24 21:37:07 +0100

    :rocket:

--- nature tour api/controllers/errorController.js
+++ nature tour api/controllers/errorController.js
@@ -7,13 +7,14 @@ const handleCastErrorDB = err => {
 
 const handleDuplicateFieldsDB = err => {
 
-  const value = err.ermsg.match(/["'])(\\?.)*?\1/)[0]
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
 
-  console.log(value)
+  console.log(value);
 
-  const message = "Duplicate field value: x. Please use another value";
+  const message = `Duplicate field value: ${value} Please use another value`;
 
   return new AppError(message, 400);
+
 };
 
 const SendErrorDev = (err, res) => {
@@ -71,6 +72,8 @@ module.exports = (err, req, res, next) => {
       if (error.name === 'CastError') error = handleCastErrorDB(error);
       
       if (error.code === 11000) error = handleDuplicateFieldsDB(error)
+
+      if (error.name === 'ValidationError') error = handleValidationErrorDB(error)
 
       sendErrorProd(error, res)
 
