73345515ba293180fb2506817ae1707b204ac935
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-24 19:03:37 +0100

    :rocket:

--- nature tour api/controllers/errorController.js
+++ nature tour api/controllers/errorController.js
@@ -1,7 +1,7 @@
 const AppError = require("../utils/appError");
 
 const handleCastErrorDB = err => {
-  const message = `Invalid ${err.path}: {err.value}`;
+  const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400)
 }
 
