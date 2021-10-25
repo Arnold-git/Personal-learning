8bfa0744639833afc464ab02bee7701c7e0dc08c
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-25 14:54:16 +0100

    :rocket:

---
+++ nature tour api/models/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+
+
+const userSchema = new mongoose.Schema({
+
+
+
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
+
