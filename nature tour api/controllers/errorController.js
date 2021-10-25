0d10064d34f274fee73c60274b38a83819bbd821
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-25 14:00:46 +0100

    :rocket:

--- nature tour api/controllers/errorController.js
+++ nature tour api/controllers/errorController.js
@@ -19,7 +19,10 @@ const handleDuplicateFieldsDB = err => {
 
 
 const handleValidationErrorDB = err => {
-  const message = "Invalid input data";
+
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data, ${errors.join('. ')}`;
   return new AppError(message, 400)
 }
 
