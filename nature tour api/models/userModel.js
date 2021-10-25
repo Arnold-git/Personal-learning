59ab233ce15f59779c6c075be6843bc26d6b8e0d
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-25 15:04:00 +0100

    :rocket:

--- nature tour api/models/userModel.js
+++ nature tour api/models/userModel.js
@@ -5,7 +5,10 @@ const validator = require('validator')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        
+        require: [true, 'Name is required'],
+        trim: true,
+        maxlength: [40, 'Name must not exceed 40 character'],
+        minlength: [10, 'Name must not be less than 10'],
     }
 
 
