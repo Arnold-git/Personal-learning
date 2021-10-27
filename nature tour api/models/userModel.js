80fd6976f1b241a53b89f58d4e6b7d13231f3ab6
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-27 11:24:47 +0100

    fixed some bugs:bug:

--- nature tour api/models/userModel.js
+++ nature tour api/models/userModel.js
@@ -5,7 +5,7 @@ const validator = require('validator')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        require: [true, 'Please tell us your name'],
+        required: [true, 'Please tell us your name'],
         trim: true,
         maxlength: [100, 'Name must not exceed 100 character'],
         
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         unique: [true, 'This email is already in use'],
-        require: [true, 'Email is a required field'],
+        required: [true, 'Email is a required field'],
         lowercase: true,
         validate: {
             validator: validator.isEmail,
@@ -25,25 +25,22 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        require: [true, 'Provide a valid password'],
+        required: [true, 'Provide a valid password'],
         minlength: [8, 'Password must be atleast 8 character']
     },
     passwordConfirm: {
         type: String,
-        require: [true, 'Please, confirm your password'],
+        required: [true, 'Please, confirm your password'],
         validate: {
             // This is only wortk for SAVE and .create
             // validator is a callback function
-            validator: function(el) {
+            validator: function (el) {
                 return el === this.password
             }, 
             message: 'password and password confirm must be the same'
         }
     }
-
-
-
-})
+});
 
 const User = mongoose.model('User', userSchema)
 
