582fe2c5b20c716a3d87bd958522cc6ff01ad052
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-27 21:41:11 +0100

    :rocket:

--- nature tour api/controllers/authController.js
+++ nature tour api/controllers/authController.js
@@ -1,10 +1,19 @@
+const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
-const User = require('./../models/userModel')
-
+const User = require('./../models/userModel');
 
 exports.signup = catchAsync( async (req, res, next) => {
 
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+         name: req.body.name,
+         email: req.body.email,
+         password: req.body.password,
+         passwordConfirmed: req.body.passwordConfirmed
+    });
+
+    
+
+    const token = jwt.sign({ id: _newUser._id})
 
     res.status(200).json({
         status: 'Success', 
@@ -13,4 +22,4 @@ exports.signup = catchAsync( async (req, res, next) => {
         }
     });
 
-});
+});  
