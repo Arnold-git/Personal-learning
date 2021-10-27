6fa9489b3362087865a04c9a282aac95c70aa433
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-27 21:46:00 +0100

    :rocket:

--- nature tour api/controllers/authController.js
+++ nature tour api/controllers/authController.js
@@ -10,10 +10,10 @@ exports.signup = catchAsync( async (req, res, next) => {
          password: req.body.password,
          passwordConfirmed: req.body.passwordConfirmed
     });
-
     
-
-    const token = jwt.sign({ id: _newUser._id})
+    const token = jwt.sign({ id: _newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIURES
+    })
 
     res.status(200).json({
         status: 'Success', 
