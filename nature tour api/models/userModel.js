48889402957f36998bd646b709854d06bcacee89
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-27 11:54:24 +0100

    fixed some bugs:bug:

--- nature tour api/models/userModel.js
+++ nature tour api/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 
 const userSchema = new mongoose.Schema({
@@ -45,10 +46,19 @@ const userSchema = new mongoose.Schema({
 
 // DOCUMENT MIDDLEWARE TO ENCRYPT PASSWORD
 
-userSchema.pre('save', function(next) {
+userSchema.pre('save', async function(next) {
+
+    // ONLY RUN FUNCTION IF PASSWORD IS ACTUALLY MODIFIED
     if(!this.isModified('password')) return next();
 
-    
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+
+
+    next()
+
+
 })
 
 const User = mongoose.model('User', userSchema)
