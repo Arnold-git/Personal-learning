9980e1cc8e82fc02d8d1b992506599e9bccc6132
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-28 21:59:16 +0100

    :rocket:

--- nature tour api/models/userModel.js
+++ nature tour api/models/userModel.js
@@ -51,10 +51,8 @@ userSchema.pre('save', async function(next) {
     if(!this.isModified('password')) return next();
 
     this.password = await bcrypt.hash(this.password, 12);
-
     this.passwordConfirm = undefined;
-
-
+    
     next()
 
 
