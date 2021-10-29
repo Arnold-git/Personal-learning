21033f5add0b021c0b0a02f666d6d1f1de8b33b8
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-29 21:46:04 +0100

    fixed validation bug :bug:

--- nature tour api/controllers/authController.js
+++ nature tour api/controllers/authController.js
@@ -2,24 +2,26 @@ const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
 
-exports.signup = catchAsync( async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
 
-    const newUser = await User.create({
+    const newUser = await User({
          name: req.body.name,
          email: req.body.email,
          password: req.body.password,
          passwordConfirm: req.body.passwordConfirm
     });
 
-    const token = jwt.sign({ id: _newUser._id}, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES
     })
+
+    const user = await newUser.save()
 
     res.status(200).json({
         status: 'Success',
         token, 
         data: {
-            user: newUser
+            user: user
         }
     });
 
