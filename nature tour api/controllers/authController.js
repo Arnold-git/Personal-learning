16af8596141cb29eb499297203952f4aedf87453
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-29 22:04:20 +0100

    fixed validation bug :bug:

--- nature tour api/controllers/authController.js
+++ nature tour api/controllers/authController.js
@@ -35,6 +35,13 @@ exports.login = (req, res, next) => {
     const {email, password } = req.body
 
     if (!email || !password) {
-        next(new AppError )
+        next(new AppError('Please provide email and password'), 400)
     }
+
+    const token = '';
+
+    res.status(200).json({
+        status: 'success',
+        token
+    })
 }
