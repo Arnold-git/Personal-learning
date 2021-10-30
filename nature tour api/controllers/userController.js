2091ff0639824b392ff20bbbd1419d7122b25811
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-10-30 10:41:10 +0100

    :rocket:

--- nature tour api/controllers/userController.js
+++ nature tour api/controllers/userController.js
@@ -1,9 +1,26 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const User = require('./../models/userModel')
+
+
+
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+
+  const users = await User.find()
+ 
+
+    res.status(200).json({
+      status: "Success",
+      results: users.length,
+      data: {
+        users
+      }
+    });
+});
+
+
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- nature tour api/routes/userRoutes.js
+++ nature tour api/routes/userRoutes.js
@@ -5,7 +5,7 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-
+router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
 
@@ -13,6 +13,8 @@ router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
+
+
 
 router
   .route('/:id')
