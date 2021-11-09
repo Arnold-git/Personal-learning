2754da49174194e758e72c6a54191d3b63fb12fe
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-11-09 22:06:08 +0100

    :rocket:

--- nature tour api/controllers/tourController.js
+++ nature tour api/controllers/tourController.js
@@ -91,11 +91,12 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 
 
-exports.deleteTour =catchAsync( async (req, res) => {
+exports.deleteTour =catchAsync( async (req, res, next) => {
     const tour = await Tour.findByIdAndDelete(req.params.id)
 
     if(!tour) {
       return next(new AppError("No tour found with that ID", 404))
+    
     }
 
     res.status(204).json({
