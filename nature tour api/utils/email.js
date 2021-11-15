2084aac3350dcc66bdc7cdc7de3a5e2449426d1c
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-11-15 13:04:46 +0100

    :rocket:

--- nature tour api/utils/email.js
+++ nature tour api/utils/email.js
@@ -2,6 +2,14 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = options => {
     // 1) create transporter
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      });
 
     // 2) Define the email option
 
