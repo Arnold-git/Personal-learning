9cd1cbbf82a8ed642bff2c6e2560e35b37d56185
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-11-19 20:22:48 +0100

    improved code structure :zap:

--- nature tour api/controllers/authController.js
+++ nature tour api/controllers/authController.js
@@ -9,15 +9,16 @@ const sendEmail = require('../utils/email');
 
 
 exports.signup = catchAsync(async (req, res, next) => {
-
-    const newUser = await User({
-         name: req.body.name,
-         email: req.body.email,
-         password: req.body.password,
-         passwordConfirm: req.body.passwordConfirm,
-         passwordChangedAt: req.body.passwordChangedAt,
-         role: req.body.role,
-    });
+   const { name, email, password, passwordConfirm, passwordChangedAt, role} = req.body;
+   
+    const newUser = await User(   
+         name,
+         email,
+         password,
+         passwordConfirm,
+         passwordChangedAt,
+         role,
+    );
 
     const token = signInToken(newUser._id)
 
@@ -37,12 +38,12 @@ exports.signup = catchAsync(async (req, res, next) => {
 exports.login = catchAsync(async (req, res, next) => {
     const {email, password } = req.body
 
-    // CHECK IF CLIENT SENDS EMAIL AND PASSWORD
+    /** CHECK IF CLIENT SENDS EMAIL AND PASSWORD */
     if (!email || !password) {
         return next(new AppError('Please provide email and password', 400));
     }
 
-    // CHECK IF USER EXISTS and PASSWORD CORRECT
+    /** CHECK IF USER EXISTS and PASSWORD CORRECT */
     const user = await User.findOne({ email }).select('+password')
 
 
@@ -63,7 +64,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.requireSignin = catchAsync(async (req, res, next) => {
 
-    // 1) Getting token and check if valid
+    /** Getting token and check if valid */
 
     let token;
 
@@ -79,12 +80,12 @@ exports.requireSignin = catchAsync(async (req, res, next) => {
         return next(new AppError('You are NOT logined in! Please, login to access', 401))
     }
 
-    // 2) token verification
+    /** 2) token verification */
 
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
    
 
-    // 3) check if user still exists
+    /** 3) check if user still exists */
 
     currentUser = await User.findById(decoded.id);
 
@@ -92,13 +93,13 @@ exports.requireSignin = catchAsync(async (req, res, next) => {
         return next(new AppError('The user belonging to this token no longer exist', 401))
     }
 
-    // 4) Check if user changed password after token was issue
+    /** 4) Check if user changed password after token was issue */
 
     if (currentUser.changedPasswordAfter(decoded.iat)) {
         return next(new AppError('User recently changed password, Please log in again', 401))
     };
 
-    // Grant access to the protected route
+    /** Grant access to the protected route */
     req.user = currentUser
     next()
 })
@@ -118,8 +119,8 @@ exports.restrictTo = (...roles) => {
 
 exports.forgotPassword = catchAsync( async (req, res, next) => {
 
-    // 1) GET USER BASED ON EMAIL
-
+    /** 1) GET USER BASED ON EMAIL
+ */
     const user = await User.findOne({ email: req.body.email })
 
     if(!user) {
@@ -128,13 +129,13 @@ exports.forgotPassword = catchAsync( async (req, res, next) => {
         ));
     };
 
-    // 2) GENERATE RANDOM RESET TOKEN
+    /** 2) GENERATE RANDOM RESET TOKEN */
 
     const resetToken = user.changedPasswordResetToken();
 
     await user.save({ validateBeforeSave: false });
 
-    // 3) SEND IT TO USER EMAIL
+    /** 3) SEND IT TO USER EMAIL */
 
     const resetURL = 
 
