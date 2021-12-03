03b211312c444235840fba54c4f77ecd8961f958
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-12-03 22:17:59 +0100

    :zap:

--- nature tour api/utils/catchAsync.js
+++ nature tour api/utils/catchAsync.js
@@ -1,3 +1,5 @@
+/** function to take catch async function */
+
 module.exports = fn => {
     // next help to catch the error
     return (req, res, next) => {
--- nature tour api/utils/signInToken.js
+++ nature tour api/utils/signInToken.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken');
 
 
+/** jwt signin required function function */
+
 module.exports = id => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES
-    })
-}
+    });
+};
