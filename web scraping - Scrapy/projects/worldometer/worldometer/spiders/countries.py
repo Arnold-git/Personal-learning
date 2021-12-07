8786d97a8e392bc5aef4224039fce8524da5bc62
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-12-07 21:13:00 +0100

    :zap:

--- web scraping - Scrapy/projects/worldometer/worldometer/spiders/countries.py
+++ web scraping - Scrapy/projects/worldometer/worldometer/spiders/countries.py
@@ -3,7 +3,7 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
@@ -13,7 +13,6 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath('.//text()').get()
             link = country.xpath('.//@href').get()
 
-        yield {
-            'country_name': name,
-            'countries_link': link
-        }
+            absolute_url = f'https://www.worldometers.info{link}'
+
+            yield scrapy.Request(url=link)
