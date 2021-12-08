ceebf5e5e92de698a313781703aecf414062b338
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-12-08 13:51:33 +0100

    :zap:

--- web scraping - Scrapy/projects/worldometer/worldometer/spiders/countries.py
+++ web scraping - Scrapy/projects/worldometer/worldometer/spiders/countries.py
@@ -1,4 +1,5 @@
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -17,4 +18,18 @@ class CountriesSpider(scrapy.Spider):
 
             # absolute_url = response.urljoin(link)
 
-            yield response.follow(url=link)
+            #convert relative link to absolute link
+           
+            yield response.follow(url=link, callback=self.parse_country)
+
+    def parse_country(self, response):
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath('.//td[2]/strong/text()').get()
+
+            yield {
+                'year': year,
+                'population': population
+            }
