d76bdce070fa89119d94a2daa0c1fea365e591ef
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-12-09 12:25:55 +0100

    :zap:

--- web scraping - Scrapy/projects/national_dept/national_dept/spiders/gdp_dept.py
+++ web scraping - Scrapy/projects/national_dept/national_dept/spiders/gdp_dept.py
@@ -7,4 +7,16 @@ class GdpDeptSpider(scrapy.Spider):
     start_urls = ['https://worldpopulationreview.com/countries/countries-by-national-debt/']
 
     def parse(self, response):
-        pass
+        countries = response.xpath('.//td/a')
+
+        for country in countries:
+            name = country.xpath('.//text()').get()
+            link = country.xpath('.//@href').get()
+
+            yield {
+                'country_name': name,
+                'link': link,
+                'link2': response.follow(url=link)
+            }
+
+        
