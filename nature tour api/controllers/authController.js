efee349490cff0868cdb028edfb81610432e8d8b
Arnold-git <arnoldighiwiyisi@gmail.com>
2021-12-31 16:49:02 +0100

    :zap:

--- nature tour api/controllers/authController.js
+++ nature tour api/controllers/authController.js
@@ -175,6 +175,13 @@ exports.forgotPassword = catchAsync( async (req, res, next) => {
 exports.resetPassword = catchAsync( async(req, res, next) => {
     //** 1) Get user based on the tokeb */
 
+    const hanshedToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+    const user = await User.findOne({
+        passwordResetToken: hanshedToken,
+        passwordResetExpires: { $gt: Date.now()}
+    });
+
     //** 2) if token has not expired, and there is user, set the new password */
     //** 3) Update changedPasswordAt property of the user */
     //** 4) Log the user in, send JWT */
