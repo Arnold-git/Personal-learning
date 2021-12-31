b8f6b44f76f179f301ba5a34150240e5ac2c2597
Arnold-git <arnoldighiwiyisi@gmail.com>
2021-12-31 16:52:15 +0100

    :zap:

--- nature tour api/controllers/authController.js
+++ nature tour api/controllers/authController.js
@@ -183,6 +183,14 @@ exports.resetPassword = catchAsync( async(req, res, next) => {
     });
 
     //** 2) if token has not expired, and there is user, set the new password */
+
+    if (!user) {
+        return next(new AppError('Token is invalid or has expired', 400))
+    };
+
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm
+    user.passwordResetToken = undefined
     //** 3) Update changedPasswordAt property of the user */
     //** 4) Log the user in, send JWT */
 
